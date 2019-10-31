de4bd219ba39315699559191f18c521376c85841
yavuz.demir <yavuz.demir@trendyol.com>
2019-10-31 19:51:02 +0300

    LeetCode Question 62

---
+++ LeetCode/src/Medium/Unique_Paths.java
@@ -0,0 +1,24 @@
+package Medium;
+
+public class Unique_Paths {
+    public static void main(String[] args) {
+        int m=7, n=3;
+        System.out.println(uniquePaths(6,4));
+
+    }
+    private static int uniquePaths(int m, int n) {
+
+        int x = m+n-2;
+        int y = Math.min(m, n)-1;
+        double result = 1;
+        double div = 1;
+        while(y>0){
+            result *= x;
+            div *= y;
+            x--;
+            y--;
+        }
+        return (int) (result/div);
+    }
+
+}
